458dff887ad231b8ff465b2f00a5d3fda6a4822b
Gerard Puig <gerard.puig@wefox.com>
2023-01-18 09:23:38 +0100

    change openApi file name

--- src/main/java/adapter/outcome/yaml/YamlSource.java
+++ src/main/java/adapter/outcome/yaml/YamlSource.java
@@ -6,6 +6,6 @@ public class YamlSource {
 
   public File getYamlFile() {
     // here maybe we get the yaml file from resources but maybe we can get it from an endpoint.
-    return new File("src/main/resources/customer-gateway.yaml");
+    return new File("src/main/resources/openApi.yaml");
   }
 }
